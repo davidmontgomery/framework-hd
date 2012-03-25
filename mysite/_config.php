15402ab64a0041cab38c670f3f282b68059f5ec0
David Montgomery <hello@davidmontgomery.co.nz>
2012-03-25 17:55:13 +1300

    Changed Config file
    Signed-off-by: David Montgomery <hello@davidmontgomery.co.nz>

--- mysite/_config.php
+++ mysite/_config.php
@@ -7,7 +7,7 @@ global $databaseConfig;
 
 /*** Local Details ***/
 require_once("conf/ConfigureFromEnv.php");
-$databaseConfig["database"] = "ss_framework-hd";
+$databaseConfig["database"] = "framework-hd";
 
 
 /*** Heyday Details ***/
